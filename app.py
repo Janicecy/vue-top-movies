b68864ece5e42871ec6c1133f58674cc38f1866c
chenying <janicecy1217@gmail.com>
2021-10-02 11:04:25 -0400

    add routes

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
-from flask import Flask
+from flask import Flask, json, jsonify
+import os
+# import imdb_spider
+from models import IMDB_Movie
 
 app = Flask(__name__)
+app.run(debug=True)
+
+
+print(os.getenv('USER_AGENT'))
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/api/movie/imdb')
+def get_imdb_movies():
+    return IMDB_Movie.objects().to_json()
+
+
+@app.route('/api/movie/imdb/ratings')
+def get_imdb_movie_ratings():
+
+    result = IMDB_Movie.objects.aggregate([
+        {
+            '$project': {
+                "_id": 0,
+                "_cls": 0
+            }
+        },
+
+        {
+            '$group': {
+                '_id': '$rating',
+                'movies': {'$push': "$$ROOT"}
+            },
+        },
+        {
+            "$sort": { "_id": 1 }
+        }
+    ])
+
+    return (jsonify(list(result)))
--- models.py
+++ models.py
@@ -1,15 +1,14 @@
 from pymongo import MongoClient
 from mongoengine import connect, Document, ListField, StringField, DecimalField
-import json 
+import json
 connect(db='top_movie', host="localhost", port=27017)
-
 
 
 # define base Movie Schema
 class Movie(Document):
     title = StringField(required=True, max_length=70)
     rating = DecimalField(precision=1)
-    genre = StringField( max_length=30)
+    genre = StringField(max_length=30)
     plot = StringField()
     directors = ListField(StringField(max_length=50))
     writers = ListField(StringField(max_length=50))
@@ -21,9 +20,9 @@ class Movie(Document):
         'allow_inheritance': True
     }
 
+
 class IMDB_Movie(Movie):
     user_review_count = StringField(max_length=10)
-
 
 
 def get_imdb_movies():
